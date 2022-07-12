6a0a00148e04393c66fe44be9b96100437c98d72
Andiikakans <108686451+Andiikakans@users.noreply.github.com>
2022-07-12 19:21:38 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -34,7 +34,7 @@ global.timezone = 'Asia/Jakarta' //  timezone wib
 global.owner = ["6281285578808","6281285578808"] //ganti agar fitur owner bisa di gunakan
 global.ownernomer = "6281285578808" // nomor wa kalian
 global.ownernomerr = "+6281285578808" //nmr wa kalian
-global.thumbnail = "./image/asz.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
+global.thumbnail = "./image/asz.jpeg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 
